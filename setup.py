cecdc1c110cfea1d0e0d8e8ab0462db949edbf2a
Francesco Berlato <fberlato@mpe.mpg.de>
2018-02-13 10:09:06 +0100

    Plot skymaps from 3ML analysis results

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='skymap',
+    version='0.1',
+    description='Plots skymaps from 3ML analysis results',
+    author='F. Berlato',
+    packages=['skymap'])
